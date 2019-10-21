0d534fe65da212c74224f27f55c5f23c9f7932e2
Milana <milana.mitaeva2018@campus-eni.fr>
2019-10-21 16:30:40 +0200

    entity Sortie

---
+++ src/Entity/Sortie.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SortieRepository")
+ */
+class Sortie
+{
+    /**
+     * @ORM\Id()
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue()
+     */
+    private $idSortie;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateHeureDebut;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $duree;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $dateLimiteInscription;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbInscriptionsMax;
+
+    /**
+     * @ORM\Column(type="string", length=500, nullable=true)
+     */
+    private $infosSortie;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Etat")
+     * @ORM\Column(type="integer")
+     */
+    private $etat;
+
+    /**
+     * @return mixed
+     */
+    public function getIdSortie()
+    {
+        return $this->idSortie;
+    }
+
+    /**
+     * @param mixed $idSortie
+     */
+    public function setIdSortie($idSortie): void
+    {
+        $this->idSortie = $idSortie;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom): void
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateHeureDebut()
+    {
+        return $this->dateHeureDebut;
+    }
+
+    /**
+     * @param mixed $dateHeureDebut
+     */
+    public function setDateHeureDebut($dateHeureDebut): void
+    {
+        $this->dateHeureDebut = $dateHeureDebut;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDuree()
+    {
+        return $this->duree;
+    }
+
+    /**
+     * @param mixed $duree
+     */
+    public function setDuree($duree): void
+    {
+        $this->duree = $duree;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateLimiteInscription()
+    {
+        return $this->dateLimiteInscription;
+    }
+
+    /**
+     * @param mixed $dateLimiteInscription
+     */
+    public function setDateLimiteInscription($dateLimiteInscription): void
+    {
+        $this->dateLimiteInscription = $dateLimiteInscription;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNbInscriptionsMax()
+    {
+        return $this->nbInscriptionsMax;
+    }
+
+    /**
+     * @param mixed $nbInscriptionsMax
+     */
+    public function setNbInscriptionsMax($nbInscriptionsMax): void
+    {
+        $this->nbInscriptionsMax = $nbInscriptionsMax;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getInfosSortie()
+    {
+        return $this->infosSortie;
+    }
+
+    /**
+     * @param mixed $infosSortie
+     */
+    public function setInfosSortie($infosSortie): void
+    {
+        $this->infosSortie = $infosSortie;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEtat()
+    {
+        return $this->etat;
+    }
+
+    /**
+     * @param mixed $etat
+     */
+    public function setEtat($etat): void
+    {
+        $this->etat = $etat;
+    }
+}
